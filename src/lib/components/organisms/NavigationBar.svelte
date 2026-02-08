7a2b26734c3e4ec7fa185a7473059ba47ae5720d
Distr0hopper <juliusarzberger@web.de>
2026-02-08 11:03:49 +0100

    feature: Added NavigationBar and Shadcn buttons

---
+++ src/lib/components/organisms/NavigationBar.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import {page} from '$app/state';
+
+	let navItems = [
+		{ name: 'Home', path: '/' },
+		{ name: 'Pictures', path: '/pictures' },
+		{ name: 'About', path: '/about' }
+	];
+</script>
+
+<nav class="flex items-center justify-between bg-neutral-900/80 px-6 py-4 backdrop-blur-sm">
+	<div class="flex-1">
+		<span class="text-xl font-bold text-white">Joli Blog</span>
+	</div>
+
+	<div class="flex space-x-6">
+			{#each navItems as item}
+				<a
+					href={item.path}
+					class="transition-colors { page.url.pathname === item.path
+          ? 'text-emerald-400 font-semibold'
+          : 'text-white hover:text-emerald-300' }"
+				>
+					{item.name}
+				</a>
+			{/each}
+	</div>
+
+	<div class="flex-1 invisible md:visible"></div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import NavigationBar from '$lib/components/organisms/NavigationBar.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<NavigationBar/>
 {@render children()}
