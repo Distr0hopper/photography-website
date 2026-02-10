c633c3c10f306dfaeeaf12c13374a51725d9f0ed
Distr0hopper <juliusarzberger@web.de>
2026-02-10 20:42:27 +0100

    feat: Added slug for travel-destinations

--- src/lib/components/common/TravelCard.svelte
+++ src/lib/components/common/TravelCard.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
+	import CardFooter from '../ui/card/card-footer.svelte';
+	import CardHeader from '../ui/card/card-header.svelte';
+	import CardAction from '../ui/card/card-action.svelte';
+	import CardTitle from '../ui/card/card-title.svelte';
+	import CardDescription from '../ui/card/card-description.svelte';
 
 	interface Props {
 		title: string;
 		description: string;
 		imageUrl: string;
-		showBadge?: boolean;
+		slug?: string;
 	}
 
-	let { title, description, imageUrl, showBadge = false }: Props = $props();
+	let { title, description, imageUrl, slug }: Props = $props();
 </script>
 
 <Card.Root class="relative mx-auto w-full max-w-sm pt-0">
@@ -19,19 +24,14 @@
 		alt="Travel destination"
 		class="relative z-20 aspect-video w-full object-cover brightness-90 dark:brightness-80"
 	/>
-	<Card.Header>
-		<Card.Action>
-			{#if showBadge}
-				<span
-					class="rounded-md bg-secondary px-2 py-1 text-xs font-medium text-secondary-foreground"
-					>Featured</span
-				>
-			{/if}
-		</Card.Action>
-		<Card.Title>{title}</Card.Title>
-		<Card.Description>{description ?? 'Description'}</Card.Description>
-	</Card.Header>
-	<Card.Footer>
-		<Button class="w-full">Visit Destination</Button>
-	</Card.Footer>
+	<CardHeader>
+		<CardAction></CardAction>
+		<CardTitle>{title}</CardTitle>
+		<CardDescription>{description ?? 'Description'}</CardDescription>
+	</CardHeader>
+	<CardFooter>
+		<Button href="/destinations/{slug ?? title.toLowerCase()}" class="w-full"
+			>Visit Destination</Button
+		>
+	</CardFooter>
 </Card.Root>
