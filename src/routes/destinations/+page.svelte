1cd6d31a6fd08d46212a54175ecfe8088bcdcd78
Distr0hopper <juliusarzberger@web.de>
2026-02-10 20:43:14 +0100

    feat: Forgot to add files

--- src/routes/destinations/+page.svelte
+++ src/routes/destinations/+page.svelte
@@ -2,11 +2,27 @@
 	import TravelCard from '$lib/components/common/TravelCard.svelte';
 </script>
 
-<div class="container mx-auto p-4">
+<div class="container mx-auto flex p-4">
 	<TravelCard
 		title="Vietnam"
 		description="Our travel route through Vietnam, from the bustling cities to the serene countryside."
 		imageUrl="https://images.unsplash.com/photo-1528127269322-539801943592?w=800"
-		showBadge={false}
+		slug="vietnam"
+	/>
+
+	<TravelCard
+		title="Italy"
+		description="Exploring the rich history  and vibrant culture of Italy, from Rome to Venice."
+		imageUrl="https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=800"
+		slug="italy"
+	/>
+
+	<TravelCard
+		title="Japan"
+		description="A journey through Japan's blend of tradition and modernity, from Tokyo to Kyoto."
+		imageUrl="https://images.unsplash.com/photo-1549692520-acc6669e2f0c?w=800"
+		slug="japan"
 	/>
 </div>
+
+<!-- Only 3 TravelCards in a Row! -->
---
+++ src/routes/destinations/[slug]/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<div class="flex h-screen items-center justify-center">
+	<h1 class="text-4xl font-bold text-neutral-200">Travel Page</h1>
+</div>
