37c70cf81a5188379878273715057f8ac882e399
Distr0hopper <juliusarzberger@web.de>
2026-02-24 18:54:04 +0100

    feat: Fetch Notion data via server for pages

---
+++ src/routes/destinations/[destination]/+page.server.ts
@@ -0,0 +1,16 @@
+import { Client } from '@notionhq/client';
+import { NOTION_API_KEY, NOTION_DATABASE_ID_VIETNAM } from '$env/static/private';
+
+const notion = new Client({ auth: NOTION_API_KEY });
+
+export async function load({ params }) {
+	const response = await notion.dataSources.query({
+		data_source_id: NOTION_DATABASE_ID_VIETNAM,
+		sorts: [{ property: 'Nr', direction: 'ascending' }]
+	});
+
+	return {
+		destination: params.destination,
+		notionData: response.results
+	};
+}
--- src/routes/destinations/[destination]/+page.svelte
+++ src/routes/destinations/[destination]/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	import { page } from '$app/state';
 	import Button from '$lib/components/ui/button/button.svelte';
+
+	let { data } = $props();
+	console.log('Notion data:', data.notionData);
 	let pageId = page.params.destination;
 </script>
 
 <div>Visiting subpage of {pageId}</div>
+<pre>{JSON.stringify(data.notionData, null, 2)}</pre>
 {#if pageId == 'vietnam'}
 	<Button href={page.url.pathname + '/ho-chi-minh'}>Ho Chi Minh</Button>
 	<Button href={page.url.pathname + '/can-tho'}>Can Tho</Button>
